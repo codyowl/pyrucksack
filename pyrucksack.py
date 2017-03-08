5e8d9eabf76cdb6fe732af707f40b72b4049d1f7
codyowl <codyowl@gmail.com>
2017-03-09 00:48:09 +0530

    Platform finder added and Initialized categorized operations :rocket:

--- pyrucksack.py
+++ pyrucksack.py
@@ -2,15 +2,32 @@ import os
 from settings import PROJECT_PATH as project_path, ROOT_PATH as root_path 
 from datetime import datetime
 import shutil
+import sys
 
-def backup_directory_creator():
+
+def platform_finder():
+    platform = sys.platform
+    if "linux" in platform:
+        backup_directory_creator(linux=1)
+    elif "mac" in platform:
+        backup_directory_creator(mac=1)
+    elif "windows" in platform:
+        backup_directory_creator(windows=1)
+
+def backup_directory_creator(windows=None, mac=None, linux=None):
     current = datetime.now()
     current_month = datetime.strftime(current, '%b')
     current_date = datetime.strftime(current, '%d')
     current_year = datetime.strftime(current, '%Y')
-    backup_root_directory_name = root_path + "/" + ("%s") % (current_month)
-    backup_directory_name = ("%s-%s") % (current_date, current_year)
-    directory_name = os.path.join(backup_root_directory_name, backup_directory_name) 
+    if linux:
+        backup_root_directory_name = root_path + "/" + ("%s") % (current_month)
+        backup_directory_name = ("%s-%s") % (current_date, current_year)
+        directory_name = os.path.join(backup_root_directory_name, backup_directory_name) 
+    elif mac:
+        pass 
+    elif windows:
+        pass
+
     #root directory creator
     if not os.path.exists(backup_root_directory_name):
         os.makedirs(backup_root_directory_name)
